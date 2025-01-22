367c9d5d4952400d2d0f2da39f8840c9225ae1e2
SNS-Brancato <sns.brancato.lab@gmail.com>
2025-01-22 14:32:53 +0100

    Update prep_ligand.sh

--- prep_ligand_protein/prep_ligand.sh
+++ prep_ligand_protein/prep_ligand.sh
@@ -10,15 +10,22 @@ for molecule_type in "${molecule_types[@]}"; do
   for number in "${molecule_numbers[@]}"; do
     # Create the filenames based on the molecule type and number
     mol_name="${molecule_type}_${number}"
+    pdb_file="${mol_name}_prep.pdb"
     mol2_file="${mol_name}_prep.mol2"
     frcmod_file="${mol_name}_prep.frcmod"
     lib_file="${mol_name}_prep.lib"
     prmtop_file="${mol_name}_prep.prmtop"
     rst7_file="${mol_name}_prep.rst7"
-    tleap_input="${type}_${num}.tleap.in"
+    tleap_input="${mol_name}.tleap.in"
+
+    # Run antechamber to generate the mol2 file
+    antechamber -i "$pdb_file" -fi pdb -o "$mol2_file" -fo mol2 -nc 1 -c bcc -at gaff2
+
+    # Run parmchk2 to generate the frcmod file
+    parmchk2 -i "$mol2_file" -f mol2 -o "$frcmod_file"
 
     # Run tleap for each molecule and number
-    cat > "tleap_input" <<EOF
+    cat > "$tleap_input" <<EOF
 source leaprc.gaff2
 M77 = loadmol2 $mol2_file
 check M77
@@ -28,7 +35,9 @@ saveamberparm M77 $prmtop_file $rst7_file
 quit
 EOF
 
-    tleap -f "tleap_input"
+    tleap -f "$tleap_input"
+
+    # Output processed molecule
     echo "Processed $mol_name"
   done
 done
